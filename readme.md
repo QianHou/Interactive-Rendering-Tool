9b8f503adad017cfb807d54eb29eea06f127c428
houqian <765613142@qq.com>
2020-05-09 16:48:59 +0800

    opengl deploy

---
+++ readme.md
@@ -0,0 +1,22 @@
+# INTERACTIVE RENDERING TOOL
+
+## 依赖
+
+- OpenGL Library安装：`sudo apt-get install libgl1-mesa-dev`
+- OpenGL Utilities安装：`sudo apt-get install libglu1-mesa-dev`
+- OpenGL Utility Toolkit安装：`sudo apt-get install libglut-dev`
+
+
+## 编译
+
+- `cd interactive_rendering_tool`
+- (首次编译)添加编译目录：`mkdir build`
+- (首次编译)添加执行权限：`sudo chmod 777 toolmake`
+- 代码编译：`./toolmake`
+
+
+## 运行
+
+- `cd interactive_rendering_tool`
+- (首次运行)添加执行权限：`sudo chmod 777 toolrun`
+- 代码编译：`./toolrun`
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/* Copyright [2020] <houqian & xiaotong> */
+#include <GL/glut.h>
+
+void draw(void) {
+  // green background
+  glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glFlush();
+}
+
+int main(int argc, char *argv[])
+{
+  /* GLUT环境初始化*/
+  glutInit(&argc, argv);
+  /* 显示模式初始化 */
+  glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+  /* 窗口大小定义 */
+  glutInitWindowSize(480, 480);
+  /* 定义窗口位置 */
+  glutInitWindowPosition(0, 0);
+  /* 窗口显示 */
+  glutCreateWindow("Interactive Rendering Tool");
+  /* 注册绘图函数 */
+  glutDisplayFunc(draw);
+  /* GLUT消息循环，开始执行程序 */
+  glutMainLoop();
+  return 0;
+}
